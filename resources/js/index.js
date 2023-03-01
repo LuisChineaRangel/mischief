3a3639a3ff11a4612dfbce3c3cd015832953fc61
IluzioDev <luischinearangel@gmail.com>
2023-03-01 13:00:02 +0000

    Updating

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  var include_meta = "/resources/html/meta.html";
+  var include_meta = "./resources/html/meta.html";
   $.get(include_meta, function (meta) {
     $("head").prepend(meta);
   });
 
-  $("header").load("/resources/html/header.html");
-  $("footer").load("/resources/html/footer.html");
+  $("header").load("./resources/html/header.html");
+  $("footer").load("./resources/html/footer.html");
 
   AOS.init();
 });
