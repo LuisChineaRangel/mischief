9e9f29f06dcfee0f024b98b0251dd9fe58372ec3
IluzioDev <luischinearangel@gmail.com>
2023-04-24 15:56:09 +0100

    UPdating

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-  $("header").load("./header.html");
-  $("footer").load("./footer.html");
+  $("header").load("header.html");
+  $("footer").load("footer.html");
 });
