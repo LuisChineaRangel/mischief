4092df257a8bcb842d40deb633b927a48dbc2520
IluzioDev <luischinearangel@gmail.com>
2023-05-15 18:37:41 +0100

    Updating

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 function loadTranslations(language) {
+  $("#languageDropdown").text(language.toUpperCase());
   $.getJSON("translations.json", function(data) {
     translations = data[language];
     changeLanguage();
@@ -19,4 +20,5 @@ $(document).ready(function () {
   $("footer").load("footer.html");
   $("#sidebar").load("sidebar.html");
   loadTranslations("en");
+  $("#languageDropdown").text("EN");
 });
