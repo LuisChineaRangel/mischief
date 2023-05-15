f91e26ca548f9dc81364b56bf31be32652bf5b8a
IluzioDev <luischinearangel@gmail.com>
2023-05-15 16:14:39 +0100

    Updating

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
+function switchLanguage(language) {
+  // Fetch the translations from the JSON file
+  // based on the selected language
+  var translations = window.translations[language];
+
+  // Update the content on the page
+  document.getElementById("title").innerText = translations.title;
+  document.getElementById("content").innerText = translations.content;
+}
+
+// Load the appropriate language on page load
+document.addEventListener("DOMContentLoaded", function () {
+  var userLanguage = getUserLanguage(); // implement a function to get user's preferred language
+  switchLanguage(userLanguage);
+});
+
 $(document).ready(function () {
   $("header").load("header.html");
   $("footer").load("footer.html");
--- js/index.js
+++ js/index.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
       $(this).find('.carousel-control-prev, .carousel-control-next').stop().fadeTo(200, 0);
     }
   );
+
+  var maxLines = 2; // Maximum number of lines to display
+  var lineHeight = parseInt($('.text-container').css('line-height')); // Get the line height of the container
+  var maxHeight = lineHeight * maxLines; // Calculate the maximum height
+  
+  $('.text-container a').each(function() {
+    var text = $(this).text();
+    var lines = Math.floor($(this).height() / lineHeight);
+    
+    if (lines > maxLines) {
+      var truncatedText = text.split(' ').slice(0, maxLines * 2).join(' '); // Adjust the number (7) to control the truncation length
+      $(this).text(truncatedText + '...');
+    }
+  });
 });
