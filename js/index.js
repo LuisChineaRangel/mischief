7c778a21669ae69bd04c9ea62b93c913f608b94a
alu0101327679 <alu0101327679@ull.edu.es>
2023-05-16 02:40:26 +0100

    en spanish

--- js/index.js
+++ js/index.js
@@ -14,20 +14,24 @@ $(document).ready(function () {
     }
   );
 
-  var maxLines = 2; // Maximum number of lines to display
-  var lineHeight = parseInt($(".text-container").css("line-height")); // Get the line height of the container
-  var maxHeight = lineHeight * maxLines; // Calculate the maximum height
+var maxLines = 2; // Maximum number of lines to display
+var lineHeight = parseInt($(".text-container").css("line-height")); // Get the line height of the container
+var maxHeight = lineHeight * maxLines; // Calculate the maximum height
 
-  $(".text-container a").each(function () {
-    var text = $(this).text();
-    var lines = Math.floor($(this).height() / lineHeight);
+var combinedText = ""; // Variable to store the combined text
 
-    if (lines > maxLines) {
-      var truncatedText = text
-        .split(" ")
-        .slice(0, maxLines * 2)
-        .join(" "); // Adjust the number (7) to control the truncation length
-      $(this).text(truncatedText + "...");
-    }
-  });
+$(".text-container p").each(function() {
+  var text = $(this).text();
+  combinedText += text + " ";
+});
+
+var lines = Math.floor($(".text-container").height() / lineHeight);
+if (lines > maxLines) {
+  var truncatedText = combinedText
+    .split(" ")
+    .slice(0, maxLines * 2)
+    .join(" "); // Adjust the number (2) to control the truncation length
+  $(".text-container").text(truncatedText + "...");
+}
+
 });
